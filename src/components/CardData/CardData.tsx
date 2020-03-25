715377d64b3b896927d5ca1a0e2b1f837cc873ee
Matias Prieto <mep.prieto@gmail.com>
2020-03-25 19:08:07 -0300

    card data: add rate

--- src/components/CardData/CardData.tsx
+++ src/components/CardData/CardData.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import './card.css';
 
 interface CardDataProps { 
-    value: number,
     title: string, 
-    classProp?: string 
+    value: number,
+    classProp?: string,
+    rate?: string
 };
 
-const CardData: React.FC<CardDataProps> = ({ value, title, classProp = null }) => (
+const CardData: React.FC<CardDataProps> = ({ value, title, classProp = null, rate = null }) => (
     <div className={`card-data ${classProp ? classProp : ''}`}>
         <span className="card-data-title">{title}</span>
         <span className="card-data-value">{value}</span>
+        {rate && <span>{`${rate} %`}</span>}
     </div>
 );
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,25 +5,35 @@ import Covid19Api from '../../services/Covid19Api';
 
 const covid19Api = new Covid19Api();
 
+const calculateRate = (confirmed: number, value: number): string => { 
+  return (value*100/confirmed).toFixed(2)
+}
 
 const Home: React.FC = () => {
 
   const [sumamryState, summaryDispatch] = useReducer(summaryReducer, initialSummaryState)
 
   useEffect(() => {
+    
     covid19Api.summary()
         .then( result => summaryDispatch({ type: 'SET_SUMMARY', summary: result}))
         .catch(error => summaryDispatch({ type: 'SET_ERROR', error: 'An error occurred while fetching summary' }));
+    
     covid19Api.getCountries()
       .then(result => console.log(result))
-      .catch(error => console.log(error))
+      .catch(error => console.log(error));
+
+    covid19Api.getDailyReport()
+      .then(result => console.log(result))
+      .catch(error => console.log(error));
+
   }, [])
 
   if (!sumamryState.summary) {
     return null;
   }
 
-  const { summary, error } = sumamryState;
+  const { summary: {confirmed, recovered, deaths}, error } = sumamryState;
 
   if (error) {
     return <p>{ error }</p>
@@ -32,9 +42,9 @@ const Home: React.FC = () => {
 
   return (
     <div className="flex flex-center-content">
-      <CardData value={summary.confirmed} title={'Confimados'}/>
-      <CardData value={summary.recovered} title={'Recuperados'}/>
-      <CardData value={summary.deaths} title={'Fallecidos'}/>
+      <CardData value={confirmed} title={'Confirmed'}/>
+      <CardData value={recovered} title={'Recovered'} rate={calculateRate(confirmed, recovered)}/>
+      <CardData value={deaths} title={'Deaths'} rate={calculateRate(confirmed, deaths)}/>
     </div>
   );
 } 
