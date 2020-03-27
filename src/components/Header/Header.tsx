a9e0e039ca3ff84a294fc7205df43a3190428a47
Matias Prieto <matias@MacBook-Pro-de-Matias.local>
2020-03-27 20:26:19 -0300

    add select serchable

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,23 @@
-import React, { useEffect, useReducer, useContext } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 import { AppContextDispatch ,SummaryContexDispatch } from '../../App';
 import covid19Api from '../../services/Covid19Api';
 import { countriesReducer, initialCountriesState } from '../../state/countries/reducer';
+import Select from 'react-select';
 import './header.css';
 
+interface SelectOption {
+    value: string,
+    label: string
+}
+
+const initialState: SelectOption = {
+    value: '',
+    label: 'All Countries'
+}
+
 const Header: React.FC = () => {
+
+    const [selectedCountry, setSelectedCountry] = useState<SelectOption>(initialState)
 
     const summaryDispatch = useContext(SummaryContexDispatch);
     const appDispatch = useContext(AppContextDispatch);
@@ -17,26 +30,28 @@ const Header: React.FC = () => {
             .catch(error => console.log(error))
     }, []);
 
-    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const handleChange = (value: any) => {
+        setSelectedCountry(value);
         appDispatch({ type: 'SET_LOADING', isLoading: true});
-        const countryCode = event.target.value;
-        covid19Api.summary(countryCode)
+        covid19Api.summary(value.value)
             .then(response => summaryDispatch({ type: 'SET_SUMMARY', summary: response}))
-            .catch(error => console.log(error))
-            .finally(() => setTimeout( () => {appDispatch({ type: 'SET_LOADING', isLoading: false})}, 1000 ));
+            .catch(error => appDispatch({ type: 'SET_ERROR', error }))
+            .finally(() => setTimeout( () => {appDispatch({ type: 'SET_LOADING', isLoading: false})}, 300 ));
+        
     }
+
+    const selectOptions: Array<SelectOption> = countriesState.map( country => { return { value: country.iso3, label: country.name }} );
+    selectOptions.unshift(initialState)
 
     return (
         <header className="flex page-header">
             <span className="title">Covid-19</span>
-            <select onChange={(event) => handleChange(event)}>
-                <option value=''>All countries</option>
-                {countriesState.map( country => country.iso3 && <option 
-                                                                    key={country.iso3} 
-                                                                    value={country.iso3}>
-                                                                        {country.name}
-                                                                </option>)}
-            </select>
+            <Select
+            isSearchable={true}
+                className="width-300"
+                value={selectedCountry}
+                onChange={handleChange}
+                options={selectOptions} />
         </header>
     );
 }
