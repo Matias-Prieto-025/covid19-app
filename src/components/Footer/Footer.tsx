4dd9ad2f963640c297b64b7c59dd0d6aa271c104
Matias Prieto <matias@MacBook-Pro-de-Matias.local>
2020-03-31 00:39:00 -0300

    change datasource on footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import './footer.css';
 const Footer: React.FC = () => (
     <footer className="footer">
         <span>Developer: Matias Prieto</span>
-        <span>Data: <a target="_blank" rel="noopener noreferrer" href="https://github.com/mathdroid/covid-19-api">Mathdroid</a></span>
+        <span>Data: <a target="_blank" rel="noopener noreferrer" href="https://github.com/novelcovid/api">NovelCOVID</a></span>
     </footer>
 );
 
