ee4576e9a53dace5dc9a6f8cea7ad90db69640f6
Matias Prieto <mep.prieto@gmail.com>
2020-04-09 19:13:39 -0300

    set pages buttons

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TableHead from './TableHeada';
 import TableBody from './TableBody'
 import './table.css';
@@ -11,27 +11,43 @@ interface ColumnItem {
 interface TableProps {
   columns: Array<ColumnItem>,
   data: Array<any>
+  itemPerPage?: number
 }
 
-const getColumnTitles = (columns: Array<ColumnItem>) => {
-  return columns.map( item => item.title );
+const getColumnTitles = (columns: Array<ColumnItem>) => columns.map( item => item.title );
+const getColumnAccessors = (columns: Array<ColumnItem>) => columns.map( item => item.accessor );
+const calculatePagesQuantity = (data: Array<any>, itemPerPage: number): number => Math.ceil(data.length / itemPerPage)
+const loadPageDate = (page: number, data: Array<any>) => {
+
 }
 
-const getColumnAccessors = (columns: Array<ColumnItem>) => {
-  return columns.map( item => item.accessor );
-}
+const Table: React.FC<TableProps> = ({ columns, data, itemPerPage = 30 }) => {
 
-const Table: React.FC<TableProps> = ({ columns, data }) => {
+  const [pages, setPages] = useState<Array<number>>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1)
+
+  useEffect(() => {
+    const cantPages = calculatePagesQuantity(data, itemPerPage);
+    const pagesArray = Array.from({ length: cantPages }, (v, k) => k+1 )
+    setPages(pagesArray);
+  }, [data, itemPerPage])
+
+  useEffect(() => {
+    loadPageDate(currentPage, data);
+  }, [currentPage, data])
 
   if (!data) {
     return <p>Empty table</p>  
   }
 
   return (
-    <table className="table">
-        <TableHead columns={getColumnTitles(columns)} />
-        <TableBody data={data} accessors={getColumnAccessors(columns)} />
-    </table>
+    <>
+      <table className="table">
+          <TableHead columns={getColumnTitles(columns)} />
+          <TableBody data={data} accessors={getColumnAccessors(columns)} />
+      </table>
+      { pages.map( page => <button key={`table-page-${page}`} onClick={() => setCurrentPage(page)}>{page}</button>) }
+    </>
   )
 }
 
