cb8bb05c70c158c56502944c22d123e425190cb2
Matias Prieto <matias@MacBook-Pro-de-Matias.local>
2020-04-15 01:26:28 -0300

    create linechart component

---
+++ src/components/Charts/Line/LineChart.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { 
+    CartesianGrid, 
+    Legend,
+    Line, 
+    LineChart, 
+    ResponsiveContainer, 
+    Tooltip, 
+    XAxis, 
+    YAxis
+} from 'recharts';
+
+interface LineProp {
+    dataKey: string,
+    color: string
+}
+
+interface LineChartProps {
+    data: ReadonlyArray<object>,
+    xAxisKey: string,
+    lines: Array<LineProp>,
+    displayTooltip?: boolean,
+    containerWidth?: number | string,
+    containerHeight?: number | string,
+}
+
+const LineChartComponent: React.FC<LineChartProps> = ({ 
+    data, 
+    lines, 
+    xAxisKey,
+    displayTooltip = true, 
+    containerWidth = '100%',
+    containerHeight = 400
+}) => {
+    return(
+        <ResponsiveContainer width={containerWidth} height={containerHeight}>
+            <LineChart data={data}>
+                <CartesianGrid strokeDasharray="1 1" />
+                <XAxis dataKey={xAxisKey}  angle={-80} textAnchor="end"/>
+                <YAxis angle={-45} />
+                { displayTooltip && <Tooltip /> }
+                <Legend verticalAlign="top" />
+                { lines.map( line => <Line 
+                                        key={`chart-line-${line.dataKey}`} 
+                                        type="monotone" 
+                                        dataKey={line.dataKey} 
+                                        stroke={line.color}  
+                                        dot={false}/>
+                )}
+            </LineChart>
+        </ResponsiveContainer>
+    )
+}
+
+export default LineChartComponent;
