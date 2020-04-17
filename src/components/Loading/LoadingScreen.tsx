552ca013a3cf3942f0e47234be8474a41ebcaa2a
Matias Prieto <matias@MacBook-Pro-de-Matias.local>
2020-04-17 11:22:25 -0300

    add spinner to loading page

--- src/components/Loading/LoadingScreen.tsx
+++ src/components/Loading/LoadingScreen.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+import { LoadingOutlined} from '@ant-design/icons';
 import './loadingScreen.css';
 
 const LoadingScreen = () => (
     <div className="loadingScreen">
-        Loading...
+        <Row gutter={[0,24]}>
+            <Col span={24}>
+                <LoadingOutlined  style={{ fontSize: 48}}/>
+            </Col>
+        </Row>
+        <Row gutter={[0,24]}>
+            <Col span={24}>
+                <p>Loading...</p>
+            </Col>
+        </Row>
     </div>
 )
 
